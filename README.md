cdbacb655c3b0fdbba1088f6402cb26e7fec85c1
derrmru <podogomacbookair@MacBook-Air.local>
2021-04-06 18:09:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Wordpress Plugin Boilerplate
+
+A straightforward boilerplate to develop a react app that can be embedded in wordpress via shortcode.
+
+## Instructions
+
+Copy folder structure to your wordpress plugins folder (/wp-content/plugins/<YOUR_PLUGIN_FOLDER>), then run npm install.
+
+Develop locally as you would a normal react app (e.g. 'npm start' when in /wp-content/plugins/<YOUR_PLUGIN_FOLDER>/derrmru-react-app).
+
+When you are ready to use within wordpress use 'npm run build' command. Webpack will export to single js and css files in build folder. The Plugin PHP file will enqueue the javascript from there.
+
+The default is to embed the app in your wordpress site via a shortcode: [react-wordpress].
+
+## Notes
+
+If you are using this more than once in the same project, you must change shortcodes and function names in react.php file, or wordpress will throw a tantrum.
---
+++ react.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: React Wordpress Plugin Boilerplate
+ * Plugin URI: https://thepetersweeney.com
+ * Version: 1.0.0
+ * Author: Peter Sweeney
+ * Author URI: https://thepetersweeney.com
+ * Description: Embed Create React App in Wordpress via plugin. Included inline using short code.
+ */
+ 
+function react_wordpress_shortcode() {
+ 
+ return '<div id="react-wordpress" ></div>';
+}
+ 
+add_shortcode('react-wordpress', 'react_wordpress_shortcode');
+ 
+function helloworld_load_assets() {
+ 
+$react_app_js  = plugin_dir_url( __FILE__ ) . 'derrmru-react-app/build/static/js/all_in_one_file.js';
+$react_app_css = plugin_dir_url( __FILE__ ) . 'derrmru-react-app/build/static/css/all_in_one_file.css'; 
+      
+    // time stops stylesheet/js caching while in development, might want to remove later  
+    $version = null; 
+    wp_enqueue_script( 'hello-world-react', $react_app_js, array(), $version, true );         
+    wp_enqueue_style( 'hello-world-react', $react_app_css, array(), $version );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'helloworld_load_assets' );
